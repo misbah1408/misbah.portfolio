0bea265d0be1c6276687d4b0f8508979d205c908
Mohammed Misbah <154675205+misbah1408@users.noreply.github.com>
2024-09-01 15:01:17 +0530

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,7 +11,7 @@ export default function () {
         <div className="socials">
             <a href="https://github.com/misbah1408" ><i className="fa-brands fa-github text-[#ffffff]"></i></a>
             <a href="https://www.linkedin.com/in/mohammed-misba-482802259/" ><i className="fa-brands fa-linkedin hover:text-[#0077B5]"></i></a>
-            <a href="https://x.com/misbah1408003?t=JPEfWd8QyBxXeQ8S-9vd8g&s=09" ><i className="fa-brands fa-twitter hover:text-[#1DA1F2]"></i></a>
+            <a href="https://x.com/misbah0814/status/1783054225901269281" ><i className="fa-brands fa-twitter hover:text-[#1DA1F2]"></i></a>
             <a href="https://www.instagram.com/___.misbah.____/" ><i className="fa-brands fa-instagram "></i></a>
             <a href="https://drive.google.com/drive/folders/1Q6vMiSwUyqnBP4OxmrWPqrikGSng2Sj_" ><img width="20" height="20" style={{marginTop:"4px"}} src="https://img.icons8.com/office/16/resume.png" alt="resume"/></a>
         </div>
